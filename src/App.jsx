54119426a34f2eb08b88a676c8f6f507ff9a308f
luklipin <l.k.lipinski@gmail.com>
2025-10-17 16:14:27 +0200

    Bg color change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-     <div className="p-8 rounded-2xl shadow-lg bg-white">
+     <div className="p-9 rounded-3xl shadow-lg bg-gray-300">
         <h1 className="text-3xl font-bold">Fake eCommerce Shop</h1>
         <p className="mt-2 text-gray-600">Looks nice !</p>
       </div>
