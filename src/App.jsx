70c1ff31591ca05964b43616a8c13d9f79fa4571
luklipin <l.k.lipinski@gmail.com>
2025-10-17 15:54:46 +0200

    Check

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Fake eCommerce shop</h1>;
+  return (
+     <div className="p-8 rounded-2xl shadow-lg bg-white">
+        <h1 className="text-3xl font-bold">Fake eCommerce Shop</h1>
+        <p className="mt-2 text-gray-600">Looks nice !</p>
+      </div>
+  )
 };
  
 export default App;
+
+
